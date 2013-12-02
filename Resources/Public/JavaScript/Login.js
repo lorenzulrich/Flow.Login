3a6261e466d61b1307d16f23cce0a1738ebdf269
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2013-12-02 07:49:39 +0000

    Trigger the use of ajaxSubmit on submitting (instead of binding the form) because binding the form caused redirects to authentication.json several times

--- Resources/Public/JavaScript/Login.js
+++ Resources/Public/JavaScript/Login.js
@@ -1,13 +1,47 @@
-$(document).ready(function() {
+function ajaxSubmit(e) {
+    var $form = $('#login-form');
+    $.ajax({
+        url: $form.attr('action'),
+        type: $form.attr('method'),
+        data: $form.serialize(),
+        contentType: "application/x-www-form-urlencoded;charset=utf-8",
+        dataType: 'json',
+        async: false,
+        beforeSend: function(arr) {
+            $('.alert').remove();
+            $form.hide();
+            $('.modal-body').append('<p align="center"><i class="icon-spin icon-spinner icon-4x muted"></i></p>');
+        },
+        error: function(data){
+            $('.modal-body').prepend('<div class="alert alert-danger"><a class="close" href="#" data-dismiss="alert">×</a><h4 class="alert-heading">Oh snap! Your server took a nose dive!</h4>Internal Server error</div>');
+            $('p .icon-spin').remove();
+            $form.show(800, 'swing');
+        },
+        success: function(response) {
+            if (response.status === 'OK') {
+                window.location = response.redirect;
+            } else if (response.status === 'FAILED') {
+                var error;
+                for (error in response.errors) {
+                    $('.modal-body').prepend('<div class="alert alert-danger"><a class="close" href="#" data-dismiss="alert">×</a>'+ response.errors[error].message +'</div>');
+                }
+                $('.control-group').addClass('error');
+                $('p .icon-spin').remove();
+                $form.show(800, 'swing');
+            }
+        }
+    });
+    return false;
+}
 
-	// @todo handle JsonP format
+$(function() {
+
 	// Link Action to get Login Panel
 	$('.login-panel').click(function(el) {
 		el.preventDefault();
 		var url = $(this).attr('href');
 		$.get(url, function(response){
 			$('#modal-login').html(response);
-			$('#modal-login').updateModal();
 		});
 	});
 
@@ -19,40 +53,4 @@ $(document).ready(function() {
 	// When DOM is ready show button
 	$('.btn').removeClass('hide');
 
-	// Make sure clicks stays in the modal
-	$('#modal-login').updateModal();
 });
-
- (function($){
- 	$.fn.updateModal = function(e) {
-		var form = $('#login-form');
-		$(this).find('form').ajaxForm({
-			type: form.attr('method'),
-			url: form.attr('action'),
-			dataType: 'json',
-			beforeSubmit: function(arr, form) {
-				$('.alert').remove();
-				form.hide();
-				$('.modal-body').append('<p align="center"><i class="icon-spin icon-spinner icon-4x muted"></i></p>');
-			},
-			error: function(data){
-				 $('.modal-body').prepend('<div class="alert alert-danger"><a class="close" href="#" data-dismiss="alert">×</a><h4 class="alert-heading">Oh snap! Your server took a nose dive!</h4>Internal Server error</div>');
-				 $('p .icon-spin').remove();
-				 form.show(800, 'swing');
-			},
-			success: function(response) {
-				if (response.status === 'OK') {
-					window.location = response.redirect;
-				} else if (response.status === 'FAILED') {
-					var error;
-					for (error in response.errors) {
-						$('.modal-body').prepend('<div class="alert alert-danger"><a class="close" href="#" data-dismiss="alert">×</a>'+ response.errors[error].message +'</div>');
-					}
-					$('.control-group').addClass('error');
-					$('p .icon-spin').remove();
-					form.show(800, 'swing');
-				}
-			}
-		 })
- 	};
- })(jQuery);
